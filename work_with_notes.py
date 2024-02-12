e9b3ab8c1a339b6d36bf8373c6ff1a1f6426738f
Dmitry <dimatimoha2014@gmail.com>
2024-02-12 16:19:18 +0300

    in class WorkWithNotes two private var - added __fields and __column_sizes; also code in method print_notes() improved by using aux methods -  __print_long_description() and __count_spaces()

--- work_with_notes.py
+++ work_with_notes.py
@@ -2,50 +2,42 @@
 
 class WorkWithNotes:
 
-    def __init__(self, fields):
-        self.__fields=fields
-
+    __fields=['id', 'Название заметки', 'Дата создания', 'Автор', 'Описание заметки']
+    __column_sizes={'id': 10, 'Название заметки': 30, 'Дата создания': 15, 'Автор':40, 'Описание заметки':50}
 
     def print_notes(self, notes):
         # fields=['id:','Фамилия:', 'Имя:', 'Телефон:', 'Описание:']
         correct_size_for_print=lambda x,n:x+''.join([' ' for i in range(n-len(x))]) # функция добавляет недостающее количество пробелов до фиксированного размера n
+        
         for k in self.__fields:
-            if k=='id':
-                print(correct_size_for_print(k,10),end='')
-            elif k=='Описание заметки':
-                print(correct_size_for_print(k,40),end='')
-            elif k=='Дата создания':
-                print(correct_size_for_print(k,20),end='')
-            else:
-                print(correct_size_for_print(k,30),end='')
-        print('\n')
+            print(correct_size_for_print(k,self.__column_sizes[k]),end='')
+        print()
         for i in notes:
             for k,v in i.items():
-                if k=='id':
-                    print(correct_size_for_print(v,9),end=' ')
-                elif k=='Описание заметки':
-                    if (len(v)>49):
-                        # print('cirrent'+v)
-                        self.print_long_data(v)
-                    else:
-                        print(correct_size_for_print(v,39),end=' ')
-                elif k=='Дата создания':
-                    print(correct_size_for_print(v,19),end=' ')
+                if k=='Описание заметки' and len(v)>self.__column_sizes[k]-1:
+                    self.__print_long_description(v)
                 else:
-                    print(correct_size_for_print(v,29),end=' ')
+                    print(correct_size_for_print(v,self.__column_sizes[k]-1),end=' ')
             print()
         print('\n')
 
 
+    def __count_spaces_num(self,key):
+        spaces_number=0
+        for i in self.__fields:
+            if i==key:
+                break
+            else:
+                spaces_number+=self.__column_sizes[i]
+        return spaces_number
 
-    def print_long_data(self, descr):
-        # spaces = [' ']*36
+    def __print_long_description(self, descr):
         count=1
         for i in range(len(descr)):
-            if (i!=39*count):
+            if (i!=(self.__column_sizes['Описание заметки']-1)*count):
                 print(descr[i], end='')
             else:
                 print(' ')
-                print(''.join([' ']*90), end='')
+                print(''.join([' ']*self.__count_spaces_num('Описание заметки')), end='')
                 print(descr[i], end='')
                 count+=1
