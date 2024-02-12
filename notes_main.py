f9a5fe6c463d38e440fab8076d15a892004d3875
Dmitry <dimatimoha2014@gmail.com>
2024-02-12 10:18:49 +0300

    in class 'Work_with_csv' vars's status changed to private; file menu_notes.py added (contain class Notes_menu)

--- notes_main.py
+++ notes_main.py
@@ -2,26 +2,26 @@ import work_with_csv
 
 
 
-fields=['id', 'head', 'body', 'date_of_creation', 'last_edit_date']
-data_csv = work_with_csv.Work_with_csv(fields, 'notes.csv')
+fields=['id', 'Название заметки', 'Описание заметки', 'Дата создания', 'Дата последнего редактирования']
+data_csv = work_with_csv.Work_with_csv(fields)
 
-#идентификатор, заголовок, тело заметки и дату/время создания или
-#последнего изменения заметки
+# идентификатор, заголовок, тело заметки и дату/время создания или
+# последнего изменения заметки
 
-# note_1=['1', 'note1', 'smth','15.01.2022','15.01.2022']
-# note_2=['2', 'note2', 'smth2','16.01.2022','17.01.2022']
-# notes =[]
+note_1=['1', 'note1', 'smth','15.01.2022','15.01.2022']
+note_2=['2', 'note2', 'smth2','16.01.2022','17.01.2022']
+notes =[]
 
-# for item in note_1, note_2:
-#     notes.append(dict(zip(fields, item)))
+for item in note_1, note_2:
+    notes.append(dict(zip(fields, item)))
 
-# data_csv.save_data_to_csv(notes)
+data_csv.save_data_to_csv(notes)
 
 
 
-notes_from_csv= data_csv.read_data_from_csv()
-if len(notes_from_csv)!=0:
-    for i in notes_from_csv:
-        print(i)
-else:
-    print("Заметок еще не было добавлено")
+# notes_from_csv= data_csv.read_data_from_csv()
+# if len(notes_from_csv)!=0:
+#     for i in notes_from_csv:
+#         print(i)
+# else:
+#     print("Заметок еще не было добавлено")
--- work_with_csv.py
+++ work_with_csv.py
@@ -3,21 +3,22 @@ import os
 
 class Work_with_csv:
 
-    def __init__(self, fields, file_name):
-        self.file_name=file_name
-        self.fields=fields
+    __file_name = 'Notes.csv'
+
+    def __init__(self, fields):
+        self.__fields=fields
 
     def save_data_to_csv(self,notes_lst):
-        with open(self.file_name,'w',encoding='utf-8', newline='') as data_file_csv:
-            writer = csv.DictWriter(data_file_csv, self.fields, delimiter=';')
+        with open(self.__file_name,'w',encoding='utf-8', newline='') as data_file_csv:
+            writer = csv.DictWriter(data_file_csv, self.__fields, delimiter=';')
             writer.writeheader()
             writer.writerows(notes_lst)
 
 
     def read_data_from_csv(self):
         notes_lst = []
-        if os.path.isfile(self.file_name):
-            with open(self.file_name,'r',encoding='utf-8', newline='') as data_file_csv: 
+        if os.path.isfile(self.__file_name):
+            with open(self.__file_name,'r',encoding='utf-8', newline='') as data_file_csv: 
                 reader = csv.DictReader(data_file_csv, delimiter=';') 
                 notes_lst=[i for i in reader]
         return notes_lst
