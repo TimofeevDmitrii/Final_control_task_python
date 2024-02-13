03f3494a4828e8da571178b3791015f6fb0d829a
Dmitry <dimatimoha2014@gmail.com>
2024-02-13 11:13:48 +0300

    in class WorkWithNotes method add_new_note() was edited; and some changes in print_notes()

--- work_with_notes.py
+++ work_with_notes.py
@@ -6,7 +6,6 @@ class WorkWithNotes:
     __column_sizes={'id': 8, 'Дата создания': 18, 'Дата изменения': 18, 'Название заметки': 40, 'Описание заметки':50}
 
     def print_notes(self, notes_lst):
-        # fields=['id:','Фамилия:', 'Имя:', 'Телефон:', 'Описание:']
         correct_size_for_print=lambda x,n:x+''.join([' ' for i in range(n-len(x))]) # функция добавляет недостающее количество пробелов до фиксированного размера n
         
         for k in self.__fields:
@@ -18,7 +17,7 @@ class WorkWithNotes:
                     self.__print_long_description(v)
                 else:
                     print(correct_size_for_print(v,self.__column_sizes[k]-1),end=' ')
-            print()
+            print('\n')
         print('\n')
 
 
@@ -53,21 +52,22 @@ class WorkWithNotes:
 
     def add_new_note(self, notes_lst):
         new_note={}
-        # fields=['id','Фамилия', 'Имя', 'Телефон', 'Описание']
         letter_symbols=[chr(i) for i in range(1040,1104)]+[chr(i) for i in range(65,91)]+[chr(i) for i in range(97,123)] # все буквенные символы кириллицы и латиницы
         new_note['id']=self.__give_id_to_new_note(notes_lst)
 
-        current_date = datetime.date.today().strftime('%d-%m-%Y')
+        current_date = datetime.date.today().strftime('%d.%m.%Y')
         new_note['Дата создания']=current_date
         new_note['Дата изменения']=current_date
 
         for i in 'Название заметки','Описание заметки':
             correct_data=False
             while not correct_data:
-                new_note[i]=input(f'Введите данные для поля "{i}": ').strip().replace(';','.') # если вдруг попала точка с запятой в поле, то будем ее менять на точку, иначе взникнет проблема при распаковке данных программой из csv файла, т.к. разделителем принят символ точка с запятой
-                if set(new_note[i])&set(letter_symbols)==set():
+                new_note[i]=input(f'Введите данные для поля "{i}": ').strip().replace(';','.') # если вдруг попала ';' в поле, то будем ее менять на точку, иначе может взникнуть проблема при распаковке данных программой
+                if set(new_note[i])&set(letter_symbols)==set():                                # из csv файла, т.к. разделителем принят ';'(на этот случай есть отстройка в DictWriter (заключает данные, содержащие ';' в ковычки), но возможно это есть не во всех версиях)
                     print(f'Поле "{i}" обязательно для заполнения и должно содержать буквенные символы')
                 else:
-                    correct_data=True    
+                    correct_data=True 
+        if (len(new_note['Название заметки'])>39):
+            new_note['Название заметки']=new_note['Название заметки'][:39]  
         notes_lst.append(new_note)
         print(f'Добавлена новая заметка (id: {new_note["id"]})')
